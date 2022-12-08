2bd7441a7b87efc7065170298ea4a17eb85f6269
Joram van den Boezem <joram@vandenboezem.nl>
2022-12-08 11:13:45 +0100

    chore: more logging

--- src/get-registry.ts
+++ src/get-registry.ts
@@ -1,23 +1,69 @@
 import type { PackageJson } from "read-pkg";
-import { DEFAULT_NPM_REGISTRY } from "./definitions/constants.js";
+import {
+  DEFAULT_NPM_REGISTRY,
+  YARNRC_FILENAME,
+} from "./definitions/constants.js";
 import type { CommonContext } from "./definitions/context.js";
 import type { Yarnrc } from "./definitions/yarnrc.js";
 
 export function getRegistry(
   { publishConfig }: PackageJson,
   { npmRegistryServer, npmPublishRegistry }: Yarnrc,
-  { env }: { env?: CommonContext["env"] }
+  {
+    env,
+    logger,
+  }: { env: CommonContext["env"]; logger: CommonContext["logger"] }
 ) {
   const publishConfigRegistry = publishConfig?.["registry"] as
     | string
     | undefined;
 
-  return (
-    publishConfigRegistry ||
-    env?.["YARN_NPM_PUBLISH_REGISTRY"] ||
-    npmPublishRegistry ||
-    env?.["YARN_NPM_REGISTRY_SERVER"] ||
-    npmRegistryServer ||
-    DEFAULT_NPM_REGISTRY
-  );
+  if (publishConfigRegistry) {
+    logger.log(
+      `Using registry "%s" from "package.json: publishConfig.registry"`,
+      publishConfigRegistry
+    );
+
+    return publishConfigRegistry;
+  }
+
+  if (env["YARN_NPM_PUBLISH_REGISTRY"]) {
+    logger.log(
+      `Using registry "%s" from environment variable YARN_NPM_PUBLISH_REGISTRY`,
+      env["YARN_NPM_PUBLISH_REGISTRY"]
+    );
+
+    return env["YARN_NPM_PUBLISH_REGISTRY"];
+  }
+
+  if (npmPublishRegistry) {
+    logger.log(
+      `Using registry "%s" from "${YARNRC_FILENAME}: npmPublishRegistry"`,
+      npmPublishRegistry
+    );
+
+    return npmPublishRegistry;
+  }
+
+  if (env["YARN_NPM_REGISTRY_SERVER"]) {
+    logger.log(
+      `Using registry "%s" from environment variable YARN_NPM_REGISTRY_SERVER`,
+      env["YARN_NPM_REGISTRY_SERVER"]
+    );
+
+    return env["YARN_NPM_REGISTRY_SERVER"];
+  }
+
+  if (npmRegistryServer) {
+    logger.log(
+      `Using registry "%s" from "${YARNRC_FILENAME}: npmRegistryServer"`,
+      npmRegistryServer
+    );
+
+    return npmRegistryServer;
+  }
+
+  logger.log(`Using default registry "%s"`, DEFAULT_NPM_REGISTRY);
+
+  return DEFAULT_NPM_REGISTRY;
 }
